7f67a65801a12d1cd18b6558d56274bed6d8f2aa
eauleaf <eauleaf@gmail.com>
2024-07-02 08:47:52 -0700

    add .pdf to filename if ocr'd file is not already a pdf

--- R/pdf_embed_ocr.R
+++ R/pdf_embed_ocr.R
@@ -9,7 +9,7 @@
 #' To see all options, in terminal type: man ocrmypdf
 #'
 #' @param path_read text: path to the pdf to ocr (uses here)
-#' @param ...  text: optional args for OCRmyPDF (https://ocrmypdf.readthedocs.io/en/latest/cookbook.html#)
+#' @param ...  text: (optional) args for OCRmyPDF (https://ocrmypdf.readthedocs.io/en/latest/cookbook.html#)
 #' @param path_save text: (optional) save location/filename.pdf (if used, argument must be specified by name)
 #'
 #' @return a path to the OCR'd output pdf file
@@ -18,6 +18,7 @@
 #'
 #' @examples \dontrun{
 #' pdf_embed_ocr('/home/user/Desktop/somefile.pdf','--deskew', '--clean')
+#' pdf_embed_ocr('/home/user/Desktop/somefile.tif')
 #' list.files(pattern = '(?i)pdf$', full.names = T, recursive = T) |> #' purrr::map(
 #' ~pdf_embed_ocr(., '--deskew', '--rotate-pages', '--threshold', '--skip-text',
 #' '--redo-ocr', '--clean','-remove-background'))
@@ -25,22 +26,23 @@
 #'
 pdf_embed_ocr <- function(path_read, ..., path_save = NULL){
 
-  if( Sys.info()[1] != 'Linux'){ stop('Embedded OCR is only implemented on Linux. Try pdf_ocr_text() instead.')}
+  if( Sys.info()[1] != 'Linux'){ stop(
+    'Embedded OCR is only implemented on Linux. Try pdftools::pdf_ocr_text() instead.'
+    )}
 
   replace_file <- FALSE
 
   path_read <- here::here(path_read)
   if(is.null(path_save)){
-    path_save <- stringr::str_replace(path_read, '(?i)\\.pdf$','_ocr.pdf')
+    path_save <- stringr::str_replace(path_read, '(?i)\\.pdf$|$','_ocr.pdf')
   } else {
     path_save <- here::here(path_save)
   }
 
 
-
   # do you need to create a delete file
   if(path_read == path_save){
-    path_save <- stringr::str_replace(path_read, '(?i)\\.pdf$','_ocr.pdf')
+    path_save <- stringr::str_replace(path_read, '(?i)\\.pdf$|$','_ocr.pdf')
     delete_folder <- file.path(dirname(path_read),'delete')
     dir.create(delete_folder, showWarnings = F)
     replace_file = TRUE
